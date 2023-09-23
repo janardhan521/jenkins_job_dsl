dc04ebc5f685f94f44232c4cafdf8dc3e653e6b1
janardhan <janardhan521@gmail.com>
2023-09-23 22:49:13 +0400

    pipeline job changes

---
+++ jenkinspipeline.groovy
@@ -0,0 +1,23 @@
+pipeline('DSL_PIPELINE_JOB'){
+    agent any
+    stages{
+      stage('Init'){
+          steps{
+            echo 'Hi, am initial stage of jenkins pipe line job'
+            echo 'exis the initil stage'
+          }
+      }
+      stage('Build'){
+          steps{
+            echo 'Hi, am Build stage of jenkins pipe line job'
+            echo 'exis the Build stage'
+          }
+      }
+      stage('Deploy'){
+          steps{
+            echo 'Hi, am Deploy stage of jenkins pipe line job'
+            echo 'exis the Deploy stage'
+          }
+      }
+    }
+}
