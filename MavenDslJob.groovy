bb224966b530387ed50dd6ce45d353b1e81fbe1c
janardhan <janardhan521@gmail.com>
2023-09-23 20:33:45 +0400

    first groovy script

---
+++ MavenDslJob.groovy
@@ -0,0 +1,17 @@
+job('DSL_MAVEN_JOB') {
+    description("Jenkin Job configuration using DSL")
+    logRotator(2, 2)
+    scm {
+        github('https://github.com/janardhan521/devops.git', 'main')
+    }
+    triggers
+    {
+        scm('* * * * *')
+    }
+    steps {
+        maven('clean test install','devops/pom.xml')
+    }
+    publishers {
+        archiveArtifacts('**/*.war')
+    }
+}
